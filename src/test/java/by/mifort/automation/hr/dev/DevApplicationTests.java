c7deb846b1cceacc89333156cb864565c484eeb7
codeSpace200 <mikhail.petrunenko@mifort.org>
2022-07-18 23:48:20 +0300

    little part of sonar fixes (2)

--- src/test/java/by/mifort/automation/hr/dev/DevApplicationTests.java
+++ src/test/java/by/mifort/automation/hr/dev/DevApplicationTests.java
@@ -1,10 +1,13 @@
 package by.mifort.automation.hr.dev;
 
+import by.mifort.automation.hr.dev.dto.CandidateDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.Timestamp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,6 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DevApplicationTests {
     @Test
     void contextLoads() {
-        assertEquals(1, 1);
+        CandidateDto expected = new CandidateDto("1", new Timestamp(2L), "status");
+        CandidateDto actual = new CandidateDto("1", new Timestamp(2L), "status");
+        assertEquals(expected, actual);
     }
 }
