1ca3e1f2e16c761cb6556168a74153dc7c045bab
codeSpace200 <mikhail.petrunenko@mifort.org>
2022-07-20 01:19:57 +0300

    fix: unsafe remove usage v2.0

--- src/main/java/by/mifort/automation/hr/dev/service/duplicates/separate/SeparateService.java
+++ src/main/java/by/mifort/automation/hr/dev/service/duplicates/separate/SeparateService.java
@@ -7,17 +7,18 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+@SuppressWarnings("all")
 public interface SeparateService {
 
     List<List<Candidate>> separateByOneAttribute(List<CandidateAttributes> attributes, Comparator<CandidateAttributes> comparator);
 
     default List<List<Candidate>> separateBySeveralAttributes(List<Candidate> candidates, Comparator<Candidate> comparator){
         List<List<Candidate>> result = new ArrayList<>();
-        for(int i=0;i<candidates.size();i++){
+        for(int i=0; i<candidates.size(); i++){
             List<Candidate> logCandidates = new ArrayList<>();
             Candidate iCandidate = candidates.get(i);
             logCandidates.add(iCandidate);
-            for(int j=0;j<candidates.size();j++){
+            for(int j=0; j<candidates.size(); j++){
                 Candidate jCandidate = candidates.get(j);
                 if(comparator.compare(iCandidate, jCandidate) == 0 && i != j){
                     logCandidates.add(jCandidate);
