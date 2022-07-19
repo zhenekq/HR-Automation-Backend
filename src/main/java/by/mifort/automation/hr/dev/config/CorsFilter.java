b771cd984c17be63aae697186a141821a77a1133
codeSpace200 <mikhail.petrunenko@mifort.org>
2022-07-20 00:34:12 +0300

    fix: make environment access endpoint

--- src/main/java/by/mifort/automation/hr/dev/config/CorsFilter.java
+++ src/main/java/by/mifort/automation/hr/dev/config/CorsFilter.java
@@ -12,9 +12,12 @@ import java.io.IOException;
 @Component
 public class CorsFilter extends OncePerRequestFilter {
 
+    public static final String SERVER_ACCESS_ENDPOINT = System.getenv("SERVER_ACCESS_ENDPOINT");
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        System.out.println("SERVER ACCESS: " + SERVER_ACCESS_ENDPOINT);
+        response.setHeader("Access-Control-Allow-Origin", SERVER_ACCESS_ENDPOINT);
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
